9cd82cd327f1f46e37604579d6ca262a158f1106
Arnold Paye <arnoldpayet@gmail.com>
2025-12-17 17:02:14 -0400

    working with controllers

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "<h1>Welcome</h1>";
-});
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/home', function () {
     return view('home');
