84f04505763350ad3439abbffad2bddc08682b45
Arnold Paye <arnoldpayet@gmail.com>
2025-12-19 09:39:59 -0400

    working with the database

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,17 @@
 use App\Http\Controllers\GreetingController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/', function() {
+    // 1. Using raw SQL queries
+    // $users = DB::select('SELECT * FROM users');
+    // dd($users);
+
+    // 2. Query builder
+    $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    dd($users);
+});
 
 Route::get('/home', function () {
     return view('home');
