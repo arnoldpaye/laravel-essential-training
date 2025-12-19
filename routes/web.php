89b3315f2e6459e38252b3ca7a08700ba3214531
Arnold Paye <arnoldpayet@gmail.com>
2025-12-19 10:00:26 -0400

    creating models and using eloquent

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\GreetingController;
 use App\Http\Controllers\WelcomeController;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
@@ -11,8 +12,21 @@ Route::get('/', function() {
     // dd($users);
 
     // 2. Query builder
-    $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
-    dd($users);
+    // $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    // dd($users);
+
+    // 3. Eloquent ORM
+    $students = Student::select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    // dd($students);
+    /* foreach($students as $student) {
+        echo $student->name."<br>";
+    } */
+
+    $student = new Student();
+    $student->id = 3;
+    $student->name = "Jane";
+    $student->email = "jane@example.com";
+    $student->save();
 });
 
 Route::get('/home', function () {
