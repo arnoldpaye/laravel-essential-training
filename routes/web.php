80947112167b512136a22ecd8945aaaadfa23544
Arnold Paye <arnoldpayet@gmail.com>
2025-12-21 06:52:22 -0400

    cleanup

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function() {
-    // Get the student model where email is john@example.com
-    $student = Student::where('email', 'john@example.com')->first();
-    dd($student);
-
-    // Update the 'status' column with the value 'active'
-    $student->status = 'active';
-
-    // Save the model
-    $student->save();
-
-    // Dump the result
-    dd($student);
+    return view('welcome');
 });
 
 Route::get('/home', function () {
