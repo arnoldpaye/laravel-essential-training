725e2dbe246e6a3e5d0cfd674dfd5408c44172ef
Arnold Paye <arnoldpayet@gmail.com>
2025-12-23 16:36:47 -0400

    create notebook resource

---
+++ app/Http/Controllers/NotebookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NotebookController extends Controller
+{
+    /*
+    * Display a listing of the resource.
+    */
+    public function index()
+    {
+
+    }
+}
