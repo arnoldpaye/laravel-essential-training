fe9e15943f8e8c6cf89547012dc5fc6571b90aa8
Arnold Paye <arnoldpayet@gmail.com>
2025-12-25 19:11:25 -0400

    create notebook views

--- app/Http/Controllers/NotebookController.php
+++ app/Http/Controllers/NotebookController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notebook;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class NotebookController extends Controller
 {
@@ -11,6 +14,37 @@ class NotebookController extends Controller
     */
     public function index()
     {
-
+        $user_id = Auth::id();
+        $notebooks = Notebook::where('user_id', $user_id)->latest('updated_at')->get();
+        return view('notebooks.index')->with('notebooks', $notebooks);
     }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('notebooks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        $notebook = new Notebook([
+            'user_id' => Auth::id(),
+            'uuid' => Str::uuid(),
+            'name' => $request->name,
+        ]);
+        $notebook->save();
+
+        return to_route('notebooks.index');
+    }
+
+    public function show(Notebook $notebook) {}
 }
