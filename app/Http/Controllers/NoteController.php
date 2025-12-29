8781e6a4461ceec6217485b7695954dd19d333b7
Arnold Paye <arnoldpayet@gmail.com>
2025-12-29 14:54:46 -0400

    show flash data to users

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -93,7 +93,7 @@ class NoteController extends Controller
             'text' => $request->text,
         ]);
 
-        return to_route('notes.show', $note);
+        return to_route('notes.show', $note)->with('success', 'Changes saved');
     }
 
     /**
@@ -107,6 +107,6 @@ class NoteController extends Controller
 
         $note->delete();
 
-        return to_route('notes.index');
+        return to_route('notes.index')->with('success', 'Note deleted');
     }
 }
